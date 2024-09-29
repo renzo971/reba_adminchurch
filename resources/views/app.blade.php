9421d6bfff2a8da2f93d9b9107ca8ee86b844d1c
Renzo Santos <orrego.st.santos@gmail.com>
2024-09-28 22:25:16 -0500

    Addminors

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,12 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('Reba_adminchurch', 'Reba') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+<link rel="shortcut icon" type="image/png" href="/favicon.png" />
         <!-- Scripts -->
         @routes
         @viteReactRefresh
